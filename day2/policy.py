046f52647198729653b47bb2b24822af299fe50e
Kenneth Rawlings <rawlink@gmail.com>
2020-12-05 14:42:53 -0800

    Add Day 2. Rename files in Day 1.

---
+++ day2/policy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import re
+
+def is_valid(pass_policy):
+    pattern = re.compile('^(\\d+)-(\\d+) (\\w): (.*)$')
+    match = pattern.match(pass_policy)
+
+    if match:
+        min = int(match.group(1))
+        max = int(match.group(2))
+        policy_char = match.group(3)
+        password = match.group(4)
+        num_chars = password.count(policy_char)
+        if num_chars >= min and num_chars <= max:
+            return True
+
+    return False
+
+def main(input):
+    lines = []
+    with open(input) as f:
+        lines = f.readlines()
+
+    valid = list(filter(is_valid, map(lambda s: s.strip(), lines)))
+
+    return len(valid)
+
+if __name__ == '__main__':
+    print(main('passwords.txt'))
---
+++ day2/policy2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import re
+
+def is_valid(pass_policy):
+    pattern = re.compile('^(\\d+)-(\\d+) (\\w): (.*)$')
+    match = pattern.match(pass_policy)
+
+    if match:
+        idx1 = int(match.group(1)) - 1
+        idx2 = int(match.group(2)) - 1
+        policy_char = match.group(3)
+        password = match.group(4)
+        if (password[idx1] == policy_char) ^ (password[idx2] == policy_char):
+            return True
+
+    return False
+
+def main(input):
+    lines = []
+    with open(input) as f:
+        lines = f.readlines()
+
+    valid = list(filter(is_valid, map(lambda s: s.strip(), lines)))
+
+    return len(valid)
+
+if __name__ == '__main__':
+    print(main('passwords.txt'))
