2ef10a565c8c1c6b1457944ed0e21a207e2e2269
Kenneth Rawlings <rawlink@gmail.com>
2020-12-05 15:21:39 -0800

    More cleanup for Day 1.

--- day1/report.py
+++ day1/report.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 def main(input, sum):
     with open(input) as f:
-        lines = f.readlines()
+        vals = [int(line.strip()) for line in f.readlines()]
 
-        vals = [int(x.strip()) for x in lines]
+    memo = set()
+    for val in vals:
+        result = sum - val
+        if result in memo:
+            print(f'SUM: {val} + {result} = {val + result}')
+            print(f'PRODUCT: {val} * {result} = {val * result}')
 
-        memo = set()
-        for val in vals:
-            result = sum - val
-
-            if result in memo:
-                print(f'SUM: {val} + {result} = {val + result}')
-                print(f'PRODUCT: {val} * {result} = {val * result}')
-                exit()
-
-            memo.add(val)
+        memo.add(val)
 
 if __name__ == '__main__':
     main('expenses.txt', 2020)
--- day1/report2.py
+++ day1/report2.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 def main(input, sum):
     with open(input) as f:
-        lines = f.readlines()
+        vals = [int(line.strip()) for line in f.readlines()]
 
-        vals = [int(x.strip()) for x in lines]
+    for idx,val in enumerate(vals[:-1]):
+        memo = set()
+        result = sum - val
 
-        for idx,val in enumerate(vals[:-1]):
-            memo = set()
-            result = sum - val
+        for val2 in vals[idx+1:]:
+            result2 = result - val2
 
-            for val2 in vals[idx+1:]:
-                result2 = result - val2
+            if result2 in memo:
+                print(f'SUM: {val} + {val2} + {result2} = {val + val2 + result2}')
+                print(f'PRODUCT: {val} * {val2} + {result2} = {val * val2 * result2}')
 
-                if result2 in memo:
-                    print(f'SUM: {val} + {val2} + {result2} = {val + val2 + result2}')
-                    print(f'PRODUCT: {val} * {val2} + {result2} = {val * val2 * result2}')
-                    exit()
-
-                memo.add(val2)
-            memo.add(val)
+            memo.add(val2)
+        memo.add(val)
 
 if __name__ == '__main__':
     main('expenses.txt', 2020)
