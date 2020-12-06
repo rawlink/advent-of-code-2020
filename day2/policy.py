8c8fea3844c9890763211ede62a6d4c46245c3ea
Kenneth Rawlings <rawlink@gmail.com>
2020-12-05 16:37:25 -0800

    Refactor Day 2 into single executable.

--- day2/policy.py
+++ day2/policy.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python3
 import re
 
-def is_valid(pass_policy):
+class Policy:
+    def __init__(self, num1, num2, char, password):
+        self.num1 = num1
+        self.num2 = num2
+        self.char = char
+        self.password = password
+
+def parse_policies(lines):
+    policies = []
     pattern = re.compile('^(\\d+)-(\\d+) (\\w): (.*)$')
-    match = pattern.match(pass_policy)
 
-    if match:
-        min = int(match.group(1))
-        max = int(match.group(2))
-        policy_char = match.group(3)
+    for line_num, line in enumerate(lines):
+        match = pattern.match(line)
+
+        if not match:
+            raise Exception(f'Invalid policy in line #{line_num}: {line}')
+
+        num1 = int(match.group(1))
+        num2 = int(match.group(2))
+        char = match.group(3)
         password = match.group(4)
-        num_chars = password.count(policy_char)
-        if num_chars >= min and num_chars <= max:
-            return True
+        policies.append(Policy(num1, num2, char, password))
 
-    return False
+    return policies
+
+def sled_policy(policy):
+    num_chars = policy.password.count(policy.char)
+    return num_chars >= policy.num1 and num_chars <= policy.num2
+
+def toboggan_policy(policy):
+    return (policy.password[policy.num1 -1] == policy.char) ^ (policy.password[policy.num2 - 1] == policy.char)
 
 def main(input):
     with open(input) as f:
         lines = [line.strip() for line in f.readlines()]
 
-    valid = list(filter(is_valid, map(lambda s: s.strip(), lines)))
+    policies = parse_policies(lines)
 
-    print(len(valid))
+    sled_valid = len(list(filter(sled_policy, policies)))
+    toboggan_valid = len(list(filter(toboggan_policy, policies)))
+
+    print(f'Valid sled policies: {sled_valid}')
+    print(f'Valid toboggan policies: {toboggan_valid}')
 
 if __name__ == '__main__':
     main('passwords.txt')
