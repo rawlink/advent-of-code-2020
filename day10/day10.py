990936a403c1f3ad129888e4f69d2f49040e32b3
Kenneth Rawlings <rawlink@gmail.com>
2020-12-13 14:50:02 -0800

    Clean up misc Day 10 code.

--- day10/day10.py
+++ day10/day10.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 from collections import deque
 
-def distribution_product(numbers):
+def part1(adapters):
     distribution = [0,0,0]
-    last = numbers[0]
+    last = adapters[0]
 
-    for number in numbers[1:]:
-        diff = number - last
+    for adapter in adapters[1:]:
+        diff = adapter - last
         distribution[diff - 1] += 1
-        last = number
+        last = adapter
     
     return distribution[0] * distribution[2]
 
-def combos(numbers):
+def part2(adapters):
     acc = deque()
-    acc.append([numbers[0], 1])
-    for number in numbers[1:]:
-        acc.append([number, acc[-1][1]])
+    acc.append([adapters[0], 1])
+    for adapter in adapters[1:]:
+        acc.append([adapter, acc[-1][1]])
         acc_len  = len(acc)
 
         # UGLY - consider using a class for accumulator members
-        if acc_len > 2 and number - acc[-3][0] <= 3:
+        if acc_len > 2 and adapter - acc[-3][0] <= 3:
             acc[-1][1] += acc[-3][1]
 
-        if acc_len > 3 and number - acc[-4][0] <= 3:
+        if acc_len > 3 and adapter - acc[-4][0] <= 3:
             acc[-1][1] += acc[-4][1]
 
         while len(acc) > 4:
@@ -33,32 +33,32 @@ def combos(numbers):
 
 def load(file):
     with open(file) as f:
-        numbers = [int(line.strip()) for line in f.readlines()]
-    numbers.append(0)
-    numbers.sort()
-    numbers.append(numbers[-1] + 3)
-    return numbers
+        adapters = [int(line.strip()) for line in f.readlines()]
+    adapters.append(0)
+    adapters.sort()
+    adapters.append(adapters[-1] + 3)
+    return adapters
     
 def main():
-    numbers = load('test1.txt')
-    test_val = distribution_product(numbers)
-    print(f'Test 1 jolt product: {test_val}')
-    assert test_val == 35
-    test_val = combos(numbers)
-    print(f'Test 1 jolt combos: {test_val}')
-    assert test_val == 8
+    adapters = load('test1.txt')
+    result = part1(adapters)
+    print(f'Test 1 jolt product: {result}')
+    assert result == 35
+    result = part2(adapters)
+    print(f'Test 1 jolt combos: {result}')
+    assert result == 8
 
-    numbers = load('test2.txt')
-    test_val = distribution_product(numbers)
-    print(f'Test 2 jolt product: {test_val}')
-    assert test_val == 220
-    test_val = combos(numbers)
-    print(f'Test 2 jolt combos: {test_val}')
-    assert test_val == 19208
+    adapters = load('test2.txt')
+    result = part1(adapters)
+    print(f'Test 2 jolt product: {result}')
+    assert result == 220
+    result = part2(adapters)
+    print(f'Test 2 jolt combos: {result}')
+    assert result == 19208
 
-    numbers = load('input.txt')
-    print(f'Jolt product: {distribution_product(numbers)}')
-    print(f'Jolt combos: {combos(numbers)}')
+    adapters = load('input.txt')
+    print(f'Jolt product: {part1(adapters)}')
+    print(f'Jolt combos: {part2(adapters)}')
 
 if __name__ == '__main__':
     main()
