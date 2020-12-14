04e37239f31311d5219f39d233d9f4b89d47c241
Kenneth Rawlings <rawlink@gmail.com>
2020-12-13 16:23:26 -0800

    Fix direction vectors for Day 12. Clean up code for Day 12. Complete Day 12 part 2.

--- day12/day12.py
+++ day12/day12.py
@@ -10,10 +10,10 @@ SOUTH = 'S'
 WEST = 'W'
 
 DIRECTIONS = {
-    'N': (1, 0),
-    'E': (0, 1),
-    'S': (-1, 0),
-    'W': (0, -1)
+    'N': (0, 1),
+    'E': (1, 0),
+    'S': (0, -1),
+    'W': (-1, 0)
 }
 
 COMPASS = {
@@ -33,23 +33,44 @@ def load(file):
 
 def rotate(rotation, instruction, value):
     if instruction != LEFT and instruction != RIGHT:
-        raise Exception(f'Unknown rotation: {rotation}')
+        raise Exception(f'Unknown rotation instruction: {instruction}')
+
+    if value % 90 != 0:
+        raise Exception(f'Invalid rotation ({value}). Rotations must be increments of 90.')
 
     direction = 0
+
     if instruction == LEFT:
         direction = -1
-    elif instruction == RIGHT:
+    else:
         direction = 1
 
     rotation += direction * value
 
-    while rotation < 0:
-        rotation += 360
-
-    while rotation >= 360:
-        rotation -= 360
+    rotation %= 360
 
     return rotation
+
+def rotate_wp(x, y, instruction, value):
+    if instruction != LEFT and instruction != RIGHT:
+        raise Exception(f'Unknown rotation instruction: {instruction}')
+
+    if instruction == LEFT:
+        instruction = RIGHT
+        value = -value
+
+    value %= 360
+
+    if value == 0:
+        return x, y
+    elif value == 90:
+        return y, -x
+    elif value == 180:
+        return -x, -y
+    elif value == 270:
+        return -y, x
+
+    raise Exception(f'Invalid rotation ({value}). Rotations must be increments of 90.')
 
 def part1(route):
     x = 0
@@ -68,19 +89,46 @@ def part1(route):
                 direction = DIRECTIONS[instruction]
             x += direction[0] * value
             y += direction[1] * value
-    
+
+    return abs(x) + abs(y)
+
+def part2(route):
+    x = 0
+    y = 0
+    wp_x = 10
+    wp_y = 1
+
+    for step in route:
+        instruction = step[0]
+        value = step[1]
+
+        if instruction == FORWARD:
+            x += wp_x * value
+            y += wp_y * value
+        elif instruction == RIGHT or instruction == LEFT:
+            (wp_x, wp_y) = rotate_wp(wp_x, wp_y, instruction, value)
+        else:
+            direction = DIRECTIONS[instruction]
+            wp_x += direction[0] * value
+            wp_y += direction[1] * value
+
     return abs(x) + abs(y)
 
 
 def main():
     route = load('test1.txt')
     manhattan = part1(route)
-    print(f'Test 1 - Manhattan: {manhattan}')
+    print(f'Test 1 - Manhattan 1: {manhattan}')
     assert manhattan == 25
+    manhattan = part2(route)
+    print(f'Test 1 - Manhattan 2: {manhattan}')
+    assert manhattan == 286
 
     route = load('input.txt')
     manhattan = part1(route)
-    print(f'Manhattan: {manhattan}')
+    print(f'Manhattan 1: {manhattan}')
+    manhattan = part2(route)
+    print(f'Manhattan 2: {manhattan}')
 
 if __name__ == '__main__':
     main()
